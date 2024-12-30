ee9a09cda02fc800c29bd53d2e756251f0034a80
Amit Schendel <amitschendel@gmail.com>
2024-12-30 11:24:13 +0200

    Adding mock main
    Signed-off-by: Amit Schendel <amitschendel@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,27 @@
+//go:build linux
+
 package main
 
-import "github.com/amitschendel/curing/pkg/executer"
+import (
+	"context"
+
+	"github.com/amitschendel/curing/pkg/config"
+	"github.com/amitschendel/curing/pkg/executer"
+)
 
 func main() {
-	_, err := executer.NewExecuter()
+	cfg, err := config.LoadConfig("config.json")
 	if err != nil {
 		panic(err)
 	}
+
+	ctx := context.Background()
+
+	executer, err := executer.NewExecuter(cfg, ctx)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start the executer (this is a blocking call)
+	executer.Run()
 }
